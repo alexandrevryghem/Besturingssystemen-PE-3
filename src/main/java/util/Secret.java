4bc52b84546a84df041456f43b536b6e65306364
Alexandre Vryghem <alexandre2013@hotmail.be>
2020-11-29 07:15:44 -0800

    added test class

---
+++ src/main/java/util/Secret.java
@@ -0,0 +1,10 @@
+package util;
+
+import java.util.Properties;
+
+public class Secret extends Credentials {
+    static public void setPass(Properties dbProperties) {
+        dbProperties.setProperty("user", "local_r0747249");
+        dbProperties.setProperty("password", "²b=SnFw8sRHn5Zw");
+    }
+}
